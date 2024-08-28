179309661a674571b47f0a41abf78c66f6322f04
Shiva Varma <shivavarma@Shivas-MacBook-Air.local>
2024-08-28 14:43:25 +0530

    Implemented this pointer

--- Library.cpp
+++ Library.cpp
@@ -9,9 +9,13 @@ private:
     string author;
 
 public:
+    Book(string t, string a) {
+        this->title = t;
+        this->author = a;
+    }
 
     void hello() {
-        cout << "Hello, I am a Book. Title: " << endl;
+        cout << "Hello, I am a Book. Title: " << this->title << endl;
     }
 };
 
@@ -21,17 +25,19 @@ private:
     int user_id;
 
 public:
+    User(int id, string n) {
+        this->user_id = id;
+        this->name = n;
+    }
 
     void hello() {
-        cout << "Hello, I am a User. Name: " << name << endl;
+        cout << "Hello, I am a User. Name: " << this->name << endl;
     }
 };
 
 int main() {
-    Book myBook;
-    User myUser;
-
-
+    Book myBook("2001", "Jatin");
+    User myUser(1, "Vinay");
     myBook.hello();
     myUser.hello();
 
